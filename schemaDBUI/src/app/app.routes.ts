2129837d0e68415667fa810052487537dcc55d79
Srija Kancha <135148645+Srija39@users.noreply.github.com>
2026-02-07 15:50:34 +0530

    Completed navbar logo is missing continuing on homepage

--- schemaDBUI/src/app/app.routes.ts
+++ schemaDBUI/src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { schemaDBRoutes } from './schema-db/schema-db.routes';
 
 export const routes: Routes = [
-  // {path:'',redirectTo:'schema-db/home',pathMatch:'full'},
+  { path: '', redirectTo: 'schema-db/home', pathMatch: 'full' },
   {
-    path: '',
+    path: 'schema-db',
     loadComponent: () =>
       import('./schema-db/schema-db.component')
         .then(m => m.SchemaDbComponent),
--- schemaDBUI/src/app/schema-db/schema-db/schema-db.component.ts
+++ schemaDBUI/src/app/schema-db/schema-db/schema-db.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-schema-db',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,RouterModule],
   templateUrl: './schema-db.component.html',
   styleUrl: './schema-db.component.scss'
 })
