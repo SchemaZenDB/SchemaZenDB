455567f4e6d56837cf8febe33c89d3656d1ef353
Srija Kancha <135148645+Srija39@users.noreply.github.com>
2026-02-17 23:16:51 +0530

    added footer in a seperate component and implemented running number functionality in homepage

--- schemaDBUI/src/app/schema-db/contact-us/contact-us.component.ts
+++ schemaDBUI/src/app/schema-db/contact-us/contact-us.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HugeiconsIconComponent } from '@hugeicons/angular';
+import { TelegramIcon } from '@hugeicons/core-free-icons';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HugeiconsIconComponent],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.scss'
+  styleUrl: './contact-us.component.scss',
 })
 export class ContactUsComponent {
-
+  TelegramIcon = TelegramIcon;
 }
