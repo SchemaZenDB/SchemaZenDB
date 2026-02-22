41eeaa7be29e55762ad23f71043aa5a9d15eae2e
Srija Kancha <135148645+Srija39@users.noreply.github.com>
2026-02-22 13:42:46 +0530

    Enhanced Contact Page

--- schemaDBUI/src/app/schema-db/contact-us/contact-us.component.ts
+++ schemaDBUI/src/app/schema-db/contact-us/contact-us.component.ts
@@ -1,15 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HugeiconsIconComponent } from '@hugeicons/angular';
-import { TelegramIcon } from '@hugeicons/core-free-icons';
+import { Mail02FreeIcons, TelegramIcon } from '@hugeicons/core-free-icons';
+import { Call02FreeIcons } from '@hugeicons/core-free-icons';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [RouterModule, HugeiconsIconComponent],
+  imports: [RouterModule, HugeiconsIconComponent, FormsModule, CommonModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
 })
 export class ContactUsComponent {
   TelegramIcon = TelegramIcon;
+  callIcon = Call02FreeIcons;
+  mailIcon = Mail02FreeIcons;
+  userName: string = '';
+  userEmail: string = '';
+  userMessage: string = '';
+
+  sendMessage() {
+    if (this.userMessage.trim() === '') {
+      alert('Please fill in all required fields before sending your message.');
+      return;
+    } else {
+      alert(
+        'Message sent! Thank you for contacting us, ' +
+          this.userName +
+          '. We will get back to you at ' +
+          this.userEmail +
+          '.',
+      );
+    }
+  }
 }
