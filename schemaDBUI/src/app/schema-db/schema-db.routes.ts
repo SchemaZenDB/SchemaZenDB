6e8d86c3779bd30418acb7952e43102c2529dc5c
Srija Kancha <135148645+Srija39@users.noreply.github.com>
2026-02-24 23:32:10 +0530

    Reorganised Folder structure worked on navbar dropdown items and services page

--- schemaDBUI/src/app/schema-db/schema-db.routes.ts
+++ schemaDBUI/src/app/schema-db/schema-db.routes.ts
@@ -1,25 +1,47 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
 export const schemaDBRoutes: Routes = [
-
-{
-  path:'home',
-  loadComponent:()=>import('./home-page/home-page.component').then(m=>m.HomePageComponent)
-},
-{
-  path:'services',
-  loadComponent:()=>import('./services-home/services-home.component').then(m=>m.ServicesHomeComponent)
-},
-{
-  path:'about',
-  loadComponent:()=>import('./about-us/about-us.component').then(m=>m.AboutUsComponent)
-},
-{
-  path:'explore',
-  loadComponent:()=>import('./explore-more/explore-more.component').then(m=>m.ExploreMoreComponent)
-},
-{
-  path:'contact',
-  loadComponent:()=>import('./contact-us/contact-us.component').then(m=>m.ContactUsComponent)
-},
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home-page/home-page.component').then(
+        (m) => m.HomePageComponent,
+      ),
+  },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./services-home/services-home.component').then(
+        (m) => m.ServicesHomeComponent,
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about-us/about-us.component').then((m) => m.AboutUsComponent),
+  },
+  {
+    path: 'explore',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./explore/explore-more/explore-more.component').then(
+            (m) => m.ExploreMoreComponent,
+          ),
+      },
+      {
+        path: 'blog',
+        loadComponent: () =>
+          import('./explore/blog/blog.component').then((m) => m.BlogComponent),
+      },
+    ],
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./contact-us/contact-us.component').then(
+        (m) => m.ContactUsComponent,
+      ),
+  },
 ];
